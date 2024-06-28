c144617dd14cd917bcaedc6bde7716db3b33bef2
0xYashAnand <0xYashAnand@gmail.com>
2024-06-28 14:53:32 +0530

    Feat : Added Get & Patch API For Company Details

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,17 +1,27 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { CompanyService } from './company.service';
 
 import { CreateCompanyDto } from './dto/index';
 
-@Controller('')
+@Controller('company')
 export class CompanyController {
-    constructor(private companyService: CompanyService) { }
+  constructor(private companyService: CompanyService) {}
 
-    @Post('createCompany')
-    createCompany(
-        @Body() dto: CreateCompanyDto
-    ) {
-        return this.companyService.createCompany(dto);
-    }
+  @Post('createCompany')
+  createCompany(@Body() dto: CreateCompanyDto) {
+    return this.companyService.createCompany(dto);
+  }
 
+  @Get(':cid')
+  getCompanyDetails(@Param('cid') companyId: string) {
+    return this.companyService.getCompanyDetails(companyId);
+  }
+
+  @Patch(':cid/updateCompany')
+  updateCompanyDetails(
+    @Param('cid') companyId: string,
+    @Body() dto: CreateCompanyDto,
+  ) {
+    return this.companyService.updateCompanyDetails(companyId, dto);
+  }
 }
--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -1,22 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, HttpException, Injectable } from '@nestjs/common';
 import { CompanyDetails } from 'libs/mongoose';
 import { CreateCompanyDto } from './dto';
+import mongoose from 'mongoose';
 
 @Injectable()
 export class CompanyService {
-
   async createCompany(dto: CreateCompanyDto) {
     try {
       const data = new CompanyDetails(dto);
-      await data.save();      
+      await data.save();
       return {
         message: 'Company created successfully',
         data: data,
       };
-    } catch (err) {
-       console.error('error in creating company:', err);
-       throw err;
-    }     
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error.getResponse();
+      }
+      throw new BadRequestException('Unable to create Company');
+    }
   }
 
+  async getCompanyDetails(companyId: string) {
+    try {
+      const data = await CompanyDetails.findById({
+        _id: new mongoose.Types.ObjectId(companyId),
+      }).lean();
+      if (!data) {
+        throw new BadRequestException('Company not found');
+      }
+      return {
+        message: 'Company details fetched successfully',
+        data: data,
+      };
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error.getResponse();
+      }
+      throw new BadRequestException(
+        'Unable to fetch Company details with that ID.',
+      );
+    }
+  }
+
+  async updateCompanyDetails(companyId: string, dto: CreateCompanyDto) {
+    try {
+      const data = await CompanyDetails.findByIdAndUpdate(
+        { _id: new mongoose.Types.ObjectId(companyId) },
+        dto,
+      );
+      if (!data) {
+        throw new BadRequestException('Company not found');
+      }
+      return {
+        message: 'Company details updated successfully',
+        data: dto,
+      };
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error.getResponse();
+      }
+      throw new BadRequestException(
+        'Unable to update Company details with that ID.',
+      );
+    }
+  }
 }
