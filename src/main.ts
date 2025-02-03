73dfbb36e6eff2559cf5caa580e3ec330df06152
0xYashAnand <0xYashAnand@gmail.com>
2025-02-03 16:52:06 +0530

    Feat: Update CORS configuration to allow requests from multiple origins

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,13 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 // Third-party packages
 import { AppModule } from './app.module';
 
-
-
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // Allow requests from your Next.js frontend
+    origin: ['http://localhost:3000', 'http://localhost:3001'], // Allow requests from your Next.js frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
   await app.listen(5000);
 }
 bootstrap();
-
-
